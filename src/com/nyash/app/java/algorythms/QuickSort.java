c28b53ebe6cc14827cdcf59c4d0ba12ccf648f45
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-05-19 04:37:08 +0700

    add quicksort

--- src/com/nyash/app/java/algorythms/QuickSort.java
+++ src/com/nyash/app/java/algorythms/QuickSort.java
@@ -4,9 +4,10 @@ import java.util.Arrays;
 
 public class QuickSort {
     public static void main(String[] args) {
-        int[] array = new int[]{64, 42, 16, 71, 15, 43, 31, 53, 24, 57, 43, 74, 55, 36, 4, 18, 6};
-        System.out.println(arrayToString(array));
-        quickSort(array, 0, array.length -1);
+//        int [] array = new int[] {64, 42, 73, 41, 32, 53, 16, 24, 57, 42, 74, 55, 36};
+//        System.out.println(arrayToString(array));
+//        quickSort(array, 0, array.length -1);
+        test2();
     }
 
     private static void quickSort(int[] array, int from, int to) {
@@ -14,9 +15,9 @@ public class QuickSort {
         if (from < to) {
             int divideIndex = partition(array, from, to);
 
-            printSortStep(array, from, to, divideIndex);
+//            printSortStep(array, from, to, divideIndex);
 
-            quickSort(array, from, divideIndex -1);
+            quickSort(array, from, divideIndex - 1);
 
             quickSort(array, divideIndex, to);
 
@@ -27,7 +28,7 @@ public class QuickSort {
 
         int rightIndex = to;
         int leftIndex = from;
-        int pivot = arr[from];
+        int pivot = arr[from + (to - from) / 2];
 
         while (leftIndex <= rightIndex) {
             while (arr[leftIndex] < pivot) {
@@ -65,10 +66,69 @@ public class QuickSort {
         return sb.toString();
     }
 
+    private static void bubbleSort(int[] array) {
+        boolean isSorted = false;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] < array[i - 1]) {
+                    int temp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = temp;
+                    isSorted = false;
+                }
+            }
+        }
+    }
+
     private static void printSortStep(int[] arr, int from, int to, int partitionIndex) {
         System.out.print(arrayToString(arr));
         System.out.print("\npartition at index: " + partitionIndex);
         System.out.print(", left: " + arrayToString(Arrays.copyOfRange(arr, from, partitionIndex)));
         System.out.println(", right: " + arrayToString(Arrays.copyOfRange(arr, partitionIndex, to + 1)) + "\n");
+    }
+
+    private static void test1() {
+        int testLen = 100000;
+
+        int[] arr1 = new int[testLen];
+        int[] arr2 = new int[testLen];
+
+        System.out.println("\n-----Random array-----");
+
+        for (int i = 0; i < testLen; i++) {
+            arr2[i] = arr1[i] = (int) Math.round(Math.random() * 10000);
+        }
+
+        System.out.println("Quick Sort: ");
+        measureTime(() -> quickSort(arr1, 0, testLen - 1));
+
+        System.out.println("Bubble Sort: ");
+        measureTime(() -> bubbleSort(arr2));
+    }
+
+    private static void test2() {
+        int testLen = 100000;
+
+        int[] arr1 = new int[testLen];
+        int[] arr2 = new int[testLen];
+
+        System.out.println("\n-----Sorted array-----");
+
+        for (int i = 0; i < testLen; i++)
+            arr2[i] = arr1[i] = i;
+
+        System.out.println("Quick Sort: ");
+        measureTime(() -> quickSort(arr1, 0, testLen - 1));
+
+        System.out.println("Bubble Sort: ");
+        measureTime(() -> bubbleSort(arr2));
+    }
+
+    private static void measureTime(Runnable task) {
+        long startTime = System.currentTimeMillis();
+        task.run();
+        long elapsed = System.currentTimeMillis() - startTime;
+        System.out.println("Elapsed time: " + elapsed + "ms");
     }
 }
